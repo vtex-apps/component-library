1e50ae79a5baec01e8a315499480c22ddbeef694
ohmyguigs <ohmyguigs@gmail.com>
2019-08-29 20:48:13 -0300

    rendering empty storybook page

--- react/index.tsx
+++ react/index.tsx
@@ -1,43 +1,24 @@
 import React from 'react'
-import addons, { mockChannel } from '@storybook/addons'
-import Channel from '@storybook/channels'
-import renderStorybookUI, { Provider } from '@storybook/ui'
+import qs from './utils/qs.js'
 
-addons.setChannel(mockChannel())
-
-class ReactProvider extends Provider {
-  public channel: Channel
-
-  public constructor() {
-    super()
-    this.channel = addons.getChannel()
-  }
-
-  public getElements(type: any) {
-    // eslint-disable-next-line no-console
-    console.log('get Elements ', type)
-    return {}
-  }
-
-  public renderPreview() {
-    return <p>This is the Preview</p>
-  }
-
-  public handleAPI(api: any) {
-    // no need to do anything for now.
-    // eslint-disable-next-line no-console
-    console.log('handler API ', api)
+declare global {
+  interface Window {
+    Qs: any
   }
 }
-
 export default class App extends React.Component {
   public componentDidMount() {
     // eslint-disable-next-line no-console
     console.log('CDM')
-    const roolEl = document.getElementById('component-library-root')
-    // eslint-disable-next-line no-console
-    console.log('root el: ', roolEl, ReactProvider)
-    renderStorybookUI(roolEl, new ReactProvider())
+    window.Qs = qs
+
+    // Storybook UI has to be imported dynamically cuz it depends on Qs being on the window
+    import('./utils/StoryBookUI').then(renderStorybookUI => {
+      import('./utils/ReactProvider').then(ReactProvider => {
+        const roolEl = document.getElementById('component-library-root')
+        renderStorybookUI.default(roolEl, new ReactProvider.default())
+      })
+    })
   }
 
   public render() {
